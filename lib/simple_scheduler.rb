43ee3ead34a944e76173c7b2d040241afa8edf0d
Brian Pattison <brian@brianpattison.com>
2016-12-06 14:27:19 -0600

    Load the rake task

--- lib/simple_scheduler.rb
+++ lib/simple_scheduler.rb
@@ -6,4 +6,10 @@ require_relative "./simple_scheduler/version"
 
 # Module for scheduling jobs at specific times using Sidekiq.
 module SimpleScheduler
+  # Load the rake task into the Rails app
+  class Railtie < Rails::Railtie
+    rake_tasks do
+      load File.join(File.dirname(__FILE__), "tasks/simple_scheduler_tasks.rake")
+    end
+  end
 end
