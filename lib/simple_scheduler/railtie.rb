7f80a4fd88d0c59d97bb63e5e197a633acc119bb
Brian Pattison <brian@brianpattison.com>
2016-12-06 16:31:13 -0600

    Fix path for loading rake task

--- lib/simple_scheduler/railtie.rb
+++ lib/simple_scheduler/railtie.rb
@@ -2,7 +2,7 @@ module SimpleScheduler
   # Load the rake task into the Rails app
   class Railtie < Rails::Railtie
     rake_tasks do
-      load File.join(File.dirname(__FILE__), "tasks/simple_scheduler_tasks.rake")
+      load File.join(File.dirname(__FILE__), "../tasks/simple_scheduler_tasks.rake")
     end
   end
 end
