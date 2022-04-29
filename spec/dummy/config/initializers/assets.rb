69a71d25bbfe32f66b3b8d2cc86f5378bc1a13a2
Kenny Lindley <kenny@simplymadeapps.com>
2022-04-29 09:03:45 -0500

    Remove assets config as it is no longer used

--- spec/dummy/config/initializers/assets.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
