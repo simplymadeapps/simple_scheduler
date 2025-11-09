cf3001976284548f577915702e9516a4a113bdbc
Brandon Medenwald <brandon@simplymadeapps.com>
2025-11-09 09:38:33 -0600

    Convert At class from inheritance directly from Time to a SimpleDelegator to time. This was required because when using time travel in testing it was completely overwriting our At class and breaking all the tests.

--- lib/simple_scheduler/at.rb
+++ lib/simple_scheduler/at.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require "delegate"
+
 module SimpleScheduler
-  # A Time class for parsing the :at option on a task into the first time it should run.
+  # A delegator that parses the :at option on a task into the first time it should run.
+  # Instead of inheriting from Time (which conflicts with ActiveSupport time helpers),
+  # this wraps a Time instance and selectively overrides hour/hour? semantics.
   #   Time.now
   #   # => 2016-12-09 08:24:11 -0600
   #   SimpleScheduler::At.new("*:30")
@@ -10,8 +14,8 @@ module SimpleScheduler
   #   # => 2016-12-10 01:00:00 -0600
   #   SimpleScheduler::At.new("Sun 0:00")
   #   # => 2016-12-11 00:00:00 -0600
-  class At < Time
-    AT_PATTERN = /\A(Sun|Mon|Tue|Wed|Thu|Fri|Sat)?\s?(?:\*{1,2}|((?:\b[0-1]?[0-9]|2[0-3]))):([0-5]\d)\z/.freeze
+  class At < SimpleDelegator
+    AT_PATTERN = /\A(Sun|Mon|Tue|Wed|Thu|Fri|Sat)?\s?(?:\*{1,2}|((?:\b[0-1]?[0-9]|2[0-3]))):([0-5]\d)\z/
     DAYS = %w[Sun Mon Tue Wed Thu Fri Sat].freeze
 
     # Error class raised when an invalid string is given for the time.
@@ -30,15 +34,13 @@ module SimpleScheduler
     def initialize(at, time_zone = nil)
       @at = at
       @time_zone = time_zone || Time.zone
-      super(parsed_time.year, parsed_time.month, parsed_time.day,
-            parsed_time.hour, parsed_time.min, parsed_time.sec, parsed_time.utc_offset)
+      super(parsed_time) # Delegate all Time methods to parsed Time instance
     end
 
-    # Always returns the specified hour if the hour was given, otherwise
-    # it returns the hour calculated based on other specified options.
+    # Returns the specified hour if present in the at string, else the delegated Time's hour.
     # @return [Integer]
     def hour
-      hour? ? at_hour : super
+      hour? ? at_hour : __getobj__.hour
     end
 
     # Returns whether or not the hour was specified in the :at string.
@@ -51,8 +53,9 @@ module SimpleScheduler
 
     def at_match
       @at_match ||= begin
-        match = AT_PATTERN.match(@at)
         raise InvalidTime, "The `at` option is required." if @at.nil?
+
+        match = AT_PATTERN.match(@at)
         raise InvalidTime, "The `at` option '#{@at}' is invalid." if match.nil?
 
         match
@@ -77,11 +80,11 @@ module SimpleScheduler
 
     def next_hour
       @next_hour ||= begin
-        next_hour = at_hour
+        h = at_hour
         # Add an additional hour if a specific hour wasn't given, if the minutes
         # given are less than the current time's minutes.
-        next_hour += 1 if next_hour?
-        next_hour
+        h += 1 if next_hour?
+        h
       end
     end
 
@@ -94,37 +97,36 @@ module SimpleScheduler
     end
 
     def parsed_day
-      parsed_day = now.beginning_of_day
+      day = now.beginning_of_day
 
       # If no day of the week is given, return today
-      return parsed_day unless at_wday?
+      return day unless at_wday?
 
       # Shift to the correct day of the week if given
-      add_days = at_wday - parsed_day.wday
-      add_days += 7 if parsed_day.wday > at_wday
-      parsed_day + add_days.days
+      add_days = at_wday - day.wday
+      add_days += 7 if day.wday > at_wday
+      day + add_days.days
     end
 
     # Returns the very first time a job should be run for the scheduled task.
     # @return [Time]
     def parsed_time
-      return @parsed_time if @parsed_time
+      return @parsed_time if defined?(@parsed_time) && @parsed_time
 
-      @parsed_time = parsed_day
+      time_object = parsed_day
       change_hour = next_hour
-
       # There is no hour 24, so we need to move to the next day
       if change_hour == 24
-        @parsed_time = 1.day.from_now(@parsed_time)
+        time_object = 1.day.from_now(time_object)
         change_hour = 0
       end
-
-      @parsed_time = @parsed_time.change(hour: change_hour, min: at_min)
+      time_object = time_object.change(hour: change_hour, min: at_min)
 
       # If the parsed time is still before the current time, add an additional day if
       # the week day wasn't specified or add an additional week to get the correct time.
-      @parsed_time += at_wday? ? 1.week : 1.day if now > @parsed_time
-      @parsed_time
+      time_object += at_wday? ? 1.week : 1.day if now > time_object
+
+      @parsed_time = time_object
     end
   end
 end
